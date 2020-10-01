db5207091b65b776651ddc0a403a327c9218597c
muhammadisa <msawjk@gmail.com>
2020-10-01 08:33:55 +0700

    Done with dao test

--- app/src/test/java/com/xoxoer/postjsonplaceholder/persistence/PostsDaoTest.kt
+++ app/src/test/java/com/xoxoer/postjsonplaceholder/persistence/PostsDaoTest.kt
@@ -2,6 +2,7 @@ package com.xoxoer.postjsonplaceholder.persistence
 
 import com.xoxoer.postjsonplaceholder.MockUtil
 import com.xoxoer.postjsonplaceholder.model.PostsItem
+import com.xoxoer.postjsonplaceholder.util.common.toWhereLikeFormat
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.Is.`is`
 import org.junit.Before
@@ -24,7 +25,9 @@ class PostsDaoTest : LocalDatabase() {
             MockUtil.mockPost(1, 2),
             MockUtil.mockPost(3, 4),
             MockUtil.mockPost(5, 6),
-            MockUtil.mockPost(7, 8)
+            MockUtil.mockPost(7, 8),
+            MockUtil.mockPostForWhereLike(9, 10, "This is body1", "kotlin 1"),
+            MockUtil.mockPostForWhereLike(11, 12, "This is body2", "java 2")
         )
         listPosts.forEach { post ->
             postsDao.insertPost(post)
@@ -32,7 +35,7 @@ class PostsDaoTest : LocalDatabase() {
     }
 
     @Test
-    fun insertAndLoadAllPostsFromRoomDatabase() {
+    fun loadAllPostsFromRoomDatabase() {
         val loadedPosts = postsDao.getPosts().blockingGet()
         loadedPosts.forEach { loadedPost ->
             assertThat(loadedPost.id, `is`(loadedPost.id))
@@ -43,12 +46,46 @@ class PostsDaoTest : LocalDatabase() {
     }
 
     @Test
-    fun insertAndLoadPostFromRoomDatabase() {
+    fun loadPostFromRoomDatabase() {
         val loadedPost = postsDao.getPost(7).blockingGet()
         assertThat(loadedPost.id, `is`(7))
         assertThat(loadedPost.userId, `is`(8))
         assertThat(loadedPost.title, `is`(MockUtil.TITLE))
         assertThat(loadedPost.body, `is`(MockUtil.BODY))
+    }
+
+    @Test
+    fun loadPostWithWhereLikeFromRoomDatabase() {
+        val loadedPostQueryOne = postsDao
+            .getPostWithQuery("kotlin".toWhereLikeFormat())
+            .blockingGet()
+
+        val loadedPostQueryTwo = postsDao
+            .getPostWithQuery("java".toWhereLikeFormat())
+            .blockingGet()
+
+        assertThat(loadedPostQueryOne.size, `is`(1))
+        loadedPostQueryOne.forEach { post ->
+            assertThat(post.id, `is`(9))
+            assertThat(post.userId, `is`(10))
+            assertThat(post.title, `is`("kotlin 1"))
+            assertThat(post.body, `is`("This is body1"))
+        }
+
+        assertThat(loadedPostQueryTwo.size, `is`(1))
+        loadedPostQueryTwo.forEach { post ->
+            assertThat(post.id, `is`(11))
+            assertThat(post.userId, `is`(12))
+            assertThat(post.title, `is`("java 2"))
+            assertThat(post.body, `is`("This is body2"))
+        }
+    }
+
+    @Test
+    fun deleteAllPostsFromDatabase() {
+        postsDao.deletePosts()
+        val loadedPosts = postsDao.getPosts().blockingGet()
+        assertThat(loadedPosts.size, `is`(0))
     }
 
 }
